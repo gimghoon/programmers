ef4c1ae83d5e99329c473103e775c6ecf7d9870f
gimghoon <nlyfrjht@gmail.com>
2024-08-27 20:41:19 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 4.27 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,15 @@
+
+class Solution {
+   public static int solution(String number) {
+		  
+	       int answer=0;
+	        for (int i = 0; i < number.length(); i++) {
+	        	answer+= number.charAt(i)-'0';
+			} 
+				
+			
+		  	
+	        
+	        return answer%9;
+	    }
+}
