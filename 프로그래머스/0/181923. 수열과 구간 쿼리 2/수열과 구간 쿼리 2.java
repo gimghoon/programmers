2546333ccb5cd07d6a68b9242a69f6b2982ed06f
gimghoon <nlyfrjht@gmail.com>
2024-09-04 17:48:45 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 4.67 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,29 +1,34 @@
 import java.util.Arrays;
 class Solution {
- public static int[] solution(int[] arr, int[][] queries) {
+
+	public static int[] solution(int[] arr, int[][] queries) {
 		int[] answer = new int[queries.length];
+		
 		
 
 		for (int j = 0; j < queries.length; j++) {
-			int[] num = new int[queries[j][1] - queries[j][0] + 1];
+			answer[j]=Integer.MAX_VALUE;
 
 			for (int i = queries[j][0]; i < queries[j][1] + 1; i++) {
+				
 				if (queries[j][2] < arr[i]) {
-					num[i - queries[j][0]] = arr[i];
-				} else {
-					num[i - queries[j][0]] = Integer.MAX_VALUE;
-				}
+					
+					answer[j]=answer[j]>arr[i]?arr[i]:answer[j];
 
 			}
+			
+			// i에 해당하는값들을 arr에서 가져온다음에 k보다 큰 값을 변수에 저장해가면서 최솟값을 저장해나가는 방식은 안되는가요?
 
 			
 			
-			Arrays.sort(num);
-			answer[j]=num[0];
 			
-			answer[j] = answer[j] == Integer.MAX_VALUE ? -1 : num[0];
+			
+			
+			
 		}
+			answer[j] = answer[j] == Integer.MAX_VALUE ? -1 : answer[j];}
 		
 		return answer;
+	
 	}
 }
