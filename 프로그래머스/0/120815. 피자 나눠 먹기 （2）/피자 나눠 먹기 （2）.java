1369a0ddb82518818aef0105c07f84478d2b1caf
gimghoon <nlyfrjht@gmail.com>
2024-10-14 13:43:20 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+         int i=1;
+        while(true){
+          
+            if(6*i%n==0){
+                break;
+                
+            }
+            i++;
+        }
+        return i;
+    }
+}
