9853ab52ea18cdbc6b2aa8d017f980e36ed9e98c
gimghoon <nlyfrjht@gmail.com>
2024-10-16 01:50:15 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,16 @@
+class Solution {
+  public int solution(int n) {
+		   int number=1;
+		   int i=1;
+		 while(true) {
+			if(number>n) {
+				return i-2
+                    ;
+			}else {
+				number*=i++;
+			}
+			 
+			 
+		 }
+}
+}
