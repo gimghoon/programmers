78d8d98b1449eab72b3aca7486585d37bddaafe9
gimghoon <nlyfrjht@gmail.com>
2024-10-29 15:58:03 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 4.18 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
@@ -1,11 +1,8 @@
 
 function solution(video_len, pos, op_start, op_end, commands) {
   function checkifopening(pos) {
-    console.log(5);
-    console.log(pos);
     return pos >= op_start && pos <= op_end ? op_end : pos;
   }
-
   function changetoclockform(pos) {
     if (+pos.slice(3, 4) >= 6) {
       //분이 60이상일떄
@@ -16,21 +13,18 @@ function solution(video_len, pos, op_start, op_end, commands) {
         String(+pos.slice(3, 4) - 6) +
         pos.slice(-1);
       if (pos.slice(2) === "0") {
-        pos = String(+pos.charAt(0) + 1) + pos.slice(2);
+        pos = String(+pos.slice(0, 1) + 1) + pos.slice(2);
       }
     }
     if (pos >= video_len) {
       return video_len;
     }
-
     if (pos.slice(3, 4) === "-") {
       // 분이 -값을 가질떄
       if (pos.slice(1, 2) === "0") {
         if (pos.slice(0, 1) === "0") {
-          console.log(1);
           pos = "00:00";
           return pos;
-          console.log(2);
         }
         pos = String(+pos.slice(0, 1) - 1) + "9:5" + pos.slice(-1);
       } else {
@@ -42,14 +36,11 @@ function solution(video_len, pos, op_start, op_end, commands) {
           pos.slice(-1);
       }
     }
-
     return pos;
   }
-
   function commands_run(commands) {
     console.log(pos);
     if (commands === "next") {
-      console.log(String(+pos.slice(3, 4) + 1));
       pos = pos.slice(0, 3) + String(+pos.slice(3, 4) + 1) + pos.slice(-1);
     } else {
       pos = pos.slice(0, 3) + String(+pos.slice(3, 4) - 1) + pos.slice(-1);
@@ -61,8 +52,6 @@ function solution(video_len, pos, op_start, op_end, commands) {
     pos = commands_run(commands[i]);
     pos = changetoclockform(pos);
     pos = checkifopening(pos);
-
-    console.log(pos);
   }
   return pos;
 }
