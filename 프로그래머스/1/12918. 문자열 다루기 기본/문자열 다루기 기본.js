a93ce7396bd5aee5ddea4124a1baab8832a9571f
gimghoon <nlyfrjht@gmail.com>
2024-10-30 15:28:36 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,7 @@
+function solution(s) {
+  if (!(s.length === 4 || s.length === 6)) return false;
+  for (let i = 0; i < s.length; i++) {
+    if (!(0 <= +s.charAt(i) && +s.charAt(i) <= 9)) return false;
+  }
+  return true;
+}
