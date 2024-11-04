09e8aa316d3f7c36e692729df751d0a76cd917dc
gimghoon <nlyfrjht@gmail.com>
2024-11-04 21:07:48 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,19 @@
+function solution(s) {
+  const numbers = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+  };
+  
+  for (const num of Object.keys(numbers)) {
+    s = s.replaceAll(numbers[num], num);
+  }
+  return +s;
+}
