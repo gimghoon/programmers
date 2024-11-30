662c2ee710697c3dfab0feccc7c00c2c99c20c31
gimghoon <nlyfrjht@gmail.com>
2024-11-30 23:57:45 +0900

    [level 0] Title: 조건 문자열, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,12 @@
+
+const operations = {
+  '>=': (n, m) => n >= m,
+  '<=': (n, m) => n <= m,
+  '>!': (n, m) => n > m,
+  '<!': (n, m) => n < m,
+};
+
+function solution(ineq, eq, n, m) {
+  const op = operations[ineq + eq];
+  return Number(op(n, m));
+}
